29fdd033a20b96fef894010c54b2958a85596bc6
PedroLP7 <113777475+PedroLP7@users.noreply.github.com>
2024-05-31 21:35:49 +0200

    first commit

---
+++ src/views/Prueba.vue
@@ -0,0 +1,49 @@
+<template lang="">
+    <div>
+
+
+        <!-- hola mundo vue  {{data}} -->
+        <div>
+
+            <p  v-if="!data">loading...</p>
+
+          
+                <p v-else> {{data}}</p>
+                </div>
+
+
+            
+        
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+
+    data() {
+        return {
+          data : null
+        }
+    },
+    methods: {
+        async  fetchDATA(){
+           await  axios.get('http://127.0.0.1:8000/api/prueba')
+         .then(response => {
+              this.data = response.data
+         })
+            .catch(error => {
+                console.log(error)
+            })
+
+        }
+       
+    },
+    mounted() {
+        this.fetchDATA()
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
